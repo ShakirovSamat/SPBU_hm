9d23498c6a405253ab9a2412f70626abddbede8b
ShakirovSamat <samatx-r@mail.ru>
2022-10-05 17:02:22 +0300

    Add initial file

---
+++ test1/task1.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+
+int insertSort(int *ar, int size)
+{
+    for(int i = 0; i < size; i++)
+    {
+        if (ar[i] % 2 == 0)
+        {
+            int key = ar[i];
+            int j = i - 1;
+            int inlast = i;
+            while (ar[j] > key || ar[j] % 2 == 1)
+            {
+                if (ar[j] % 2 == 0)
+                {
+                    ar[inlast] = ar[j];
+                    inlast = j;
+                }
+                j--;
+            }
+            ar[inlast] = key;
+        }
+    }
+
+}
+
+int test()
+{
+    int array[10] = {4, 1, 2, 5, 3, 8, 9 ,10, 7, 6};
+    insertSort(array, 10);
+    int last = 0;
+    int cur = 0;
+    for(int i = 0; i < 10; ++i)
+    {
+        if(array[i] % 2 == 0)
+        {
+            last = cur;
+            cur = array[i];
+            if (last > cur)
+            {
+                return 1;
+            }
+        }
+    }
+    for(int i = 0; i < 10; ++i)
+    {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+    return 0;
+}
+
+int main()
+{
+    if (test() == 1)
+    {
+        printf("Program works incorrectly\n");
+    }
+    else{
+        printf("Program works correctly\n");
+    }
+
+    int array[10] = {4, 1, 2, 5, 3, 8, 9 ,10, 7, 6};
+    
+    insertSort(array, 10);
+
+    for(int i = 0; i < 10; ++i)
+    {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+    return 0;
+}
