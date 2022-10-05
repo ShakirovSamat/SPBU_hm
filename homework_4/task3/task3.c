e193b272c4bd68f515ce7f123b697770cb1bf46a
ShakirovSamat <samatx-r@mail.ru>
2022-10-05 22:21:26 +0300

    Added skeleton of program

---
+++ homework_4/task3/task3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main()
+{
+    int comand = -1;
+    printf("This is a phone book\nComands:\n0 - Exit\n1 - Add person\n2 - Show list of all people\n3 - Find phone number by name"
+    "\n4 - Find nume by phone number\n5 - Save added data\n");
+    scanf("%d", &comand);
+    while (comand != 0)
+    {
+        
+        switch (comand)
+        {
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            case 4:
+
+                break;
+            case 5:
+
+                break;
+            default:
+                printf("This comand doesn't exists\n"); 
+        }
+        scanf("%d", &comand);
+    }
+    
+    
+    return 0;
+}
