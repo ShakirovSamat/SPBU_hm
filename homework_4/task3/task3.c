798b0bbd86cdc6eb51e8e7c542f87706dfe752e1
ShakirovSamat <samatx-r@mail.ru>
2022-10-09 18:56:03 +0300

    add printf("Enter comand")

--- homework_4/task3/task3.c
+++ homework_4/task3/task3.c
@@ -5,6 +5,7 @@ int main()
     int comand = -1;
     printf("This is a phone book\nComands:\n0 - Exit\n1 - Add person\n2 - Show list of all people\n3 - Find phone number by name"
     "\n4 - Find nume by phone number\n5 - Save added data\n");
+    printf("Enter comand: ");
     scanf("%d", &comand);
     while (comand != 0)
     {
@@ -29,6 +30,7 @@ int main()
             default:
                 printf("This comand doesn't exists\n"); 
         }
+        printf("Enter comand: ");
         scanf("%d", &comand);
     }
     
