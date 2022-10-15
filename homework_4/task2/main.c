a2c33cb224a4d6a11a1e15d537f88f8cb41d62b7
ShakirovSamat <samatx-r@mail.ru>
2022-10-15 16:34:56 +0300

    add test

--- homework_4/task2/main.c
+++ homework_4/task2/main.c
@@ -2,12 +2,31 @@
 #include "sorts.h"
 
 
+int test()
+{
+    int nums[10] = {5, 6 ,1 ,2, 3, 9 ,7, 8, 4, 10};
+    quickSort(nums, 0, 9);
+    for (int i = 0; i < 9; ++i)
+    {
+        if (nums[i] > nums[i + 1]);
+        {
+            return -1;
+        }
+    }
+    return 0;
+}
+
 int main()
 {
+    if (!test())
+    {
+        return -1;
+    }
+
     FILE *fp = fopen("nums.txt", "r");
     if (fp == NULL)
     {
-        return 1;
+        return -1;
     }
 
     char num[100] = "";
@@ -15,7 +34,7 @@ int main()
     int ln = 0;
 
     fscanf(fp, "%d", &ln);
-    for(int i = 0; i < ln; ++i)
+    for (int i = 0; i < ln; ++i)
     {
         fscanf(fp, "%d", &array[i]);
     }
@@ -23,9 +42,11 @@ int main()
 
     quickSort(array, 0, ln - 1);
 
-    for(int i = 0; i < ln; ++i)
+    for (int i = 0; i < ln; ++i)
     {
         printf("%d ", array[i]);
     }
     printf("\n");
+
+    return 0;
 }
