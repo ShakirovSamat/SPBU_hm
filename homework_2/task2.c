de497dd65f11bcd04bd24e2fc6bbc2853c7a5af7
ShakirovSamat <samatx-r@mail.ru>
2022-12-16 16:51:18 +0300

    fixed issues

--- homework_2/task2.c
+++ homework_2/task2.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
 
-float linPow(float num, int exponent)
+float linearPower(float num, int exponent)
 {
+    if (num == 0 && exponent == 0)
+    {
+        return 1;
+    }
     float res = 1;
     if (exponent >= 0)
     {
@@ -24,27 +30,18 @@ float linPow(float num, int exponent)
 float binPow(float num, int exponent)
 {
     float res = 1;
+
+    bool isPositiveExponent = true;
     if (exponent < 0)
     {
-        exponent -= 2*exponent;
-        while (exponent != 0)
-        {  
-            if (exponent & 1) 
-            {
-                res /= num;
-                --exponent;
-            }
-            else
-            {
-                num *= num;
-                exponent >>= 1;
-            }
-        }
-        return res;
+        isPositiveExponent = false;
     }
+
+    exponent = abs(exponent);
+
     while (exponent)
-    {  
-        if (exponent & 1) 
+    {
+        if (exponent & 1)
         {
             res *= num;
             --exponent;
@@ -55,21 +52,25 @@ float binPow(float num, int exponent)
             exponent >>= 1;
         }
     }
+    if (!isPositiveExponent)
+    {
+        res = 1 / res;
+    }
     return res;
 }
 
-int main() 
+int main()
 {
-    
+
     float num = 0;
     int exponent = 0;
 
-    printf("This program calculates the power of a number\nEnter num = ");
+    printf("This program calculates the power of a number\nEnter a number = ");
     scanf_s("%f", &num);
     printf("Enter exponent = ");
     scanf_s("%d", &exponent);
-    
-    printf("Line Pow = %f \n", linPow(num, exponent));
-    printf("Bin Pow = %f", binPow(num, exponent));
+
+    printf("Linear Power = %f \n", linearPower(num, exponent));
+    printf("Binary Power = %f", binPow(num, exponent));
     return 0;
 }
