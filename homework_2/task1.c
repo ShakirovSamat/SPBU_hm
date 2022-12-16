eb243b113a683806c38007105f7d312410602834
ShakirovSamat <samatx-r@mail.ru>
2022-12-16 16:36:01 +0300

    fixed issues

--- homework_2/task1.c
+++ homework_2/task1.c
@@ -6,33 +6,32 @@ int recFib(int num)
     {
         return 1;
     }
-    return recFib(num - 2 ) + recFib(num - 1);
+    return recFib(num - 2) + recFib(num - 1);
 }
 int iterFib(int num)
 {
-    int last = 1;
-    int first = 1;
-    for(int i = 2; i <= num; i++)
+    int current = 1;
+    int previous = 1;
+    for (int i = 2; i <= num; i++)
     {
-        int temp = first;
-        first = first + last;
-        last = temp;
+        int temp = current;
+        current = previous + current;
+        previous = temp;
     }
-    return first;
-
+    return current;
 }
 
-int main() 
+int main()
 {
     int num = 0;
-    
-    printf("This program finds fibonacci number\n");
+
+    printf("This program finds Fibonacci number\n");
+    int isNum = -1;
     do
     {
         printf("Num should be >= 0. Enter num = ");
-        int isNum = scanf_s("%d", &num);
-    } while(num < 0 && isNum == 1);
-
+        isNum = scanf_s("%d", &num);
+    } while (num < 0 && isNum == 1);
 
     printf("By iteractive fuction = %d \n", iterFib(num));
     printf("By recursive function = %d", recFib(num));
