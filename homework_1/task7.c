14cdc6c3b2a66f6c1d8379949c0058da8a05d12a
ShakirovSamat <samatx-r@mail.ru>
2022-12-21 16:11:18 +0300

    fixed output

--- homework_1/task7.c
+++ homework_1/task7.c
@@ -2,43 +2,46 @@
 #include <math.h>
 #include <stdbool.h>
 
+#define MAX_NUM 100000
+
 int main()
 {
 	int lastNum = 1;
 
-	printf("The program finds simple numbers <= n\n");
+	printf("The program finds simple numbers <= n. n should be less than %d\n", MAX_NUM + 1);
 
-	while (lastNum < 2)
+	while (lastNum < 2 || lastNum > MAX_NUM)
 	{
-		printf("N should b >= 2. Enter n = ");
+		printf("n should b >= 2. Enter n = ");
 		scanf_s("%d", &lastNum);
 	}
 
-	int simpleNums[100000] = {2};
+	int simpleNums[MAX_NUM + 1] = {2};
 	int curIndex = 0;
-	for(int num = 3; num <= lastNum; num += 2)
+	for (int num = 3; num <= lastNum; num += 2)
 	{
 		bool isSimple = true;
 		int sqrtOfNum = (int)sqrt(num);
-		for(int dIndex = 1; dIndex < curIndex; ++dIndex)
+		for (int dIndex = 1; dIndex < curIndex; ++dIndex)
 		{
-			if(sqrtOfNum < simpleNums[dIndex])
+			if (sqrtOfNum < simpleNums[dIndex])
 			{
 				break;
 			}
-			if(num % simpleNums[dIndex] == 0)
+			if (num % simpleNums[dIndex] == 0)
 			{
 				isSimple = false;
 				break;
 			}
 		}
-		if (isSimple){
+		if (isSimple)
+		{
 			curIndex++;
 			simpleNums[curIndex] = num;
 		}
 	}
 
-	for(int i = 0; i <= curIndex; ++i)
+	for (int i = 0; i <= curIndex; ++i)
 	{
 		printf("%d ", simpleNums[i]);
 	}
