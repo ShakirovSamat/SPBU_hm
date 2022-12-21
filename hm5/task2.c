0009b2e5dafe59b727604def092bf2048cd11d43
ShakirovSamat <samatx-r@mail.ru>
2022-12-21 18:52:45 +0300

    just save

--- hm5/task2.c
+++ hm5/task2.c
@@ -1,10 +1,95 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include "stack.h"
+#include "stack.h"
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
+typedef struct StackElement
+{
+    int value;
+    struct StackElement *next;
+} StackElement;
+
+typedef struct Stack
+{
+    StackElement *top;
+} Stack;
+
+Stack *createStack()
+{
+    return calloc(1, sizeof(Stack));
+}
+
+int pushStack(Stack *stack, int value)
+{
+    if (stack == NULL)
+    {
+        return -1;
+    }
+
+    StackElement *newEleemnt = calloc(1, sizeof(StackElement));
+    newEleemnt->value = value;
+
+    if (stack->top == NULL)
+    {
+        stack->top = calloc(1, sizeof(StackElement));
+        stack->top = newEleemnt;
+        return 0;
+    }
+
+    newEleemnt->next = stack->top;
+    stack->top = newEleemnt;
+    return 0;
+}
+
+int popStack(Stack *stack, int *res)
+{
+    if (stack == NULL)
+    {
+        return -1;
+    }
+
+    *res = stack->top->value;
+    stack->top = stack->top->next;
+    return 0;
+}
+
+int deleteStack(Stack *stack)
+{
+    if (stack == NULL)
+    {
+        return -1;
+    }
+
+    StackElement *curElement = calloc(1, sizeof(StackElement));
+    curElement = stack->top;
+
+    while (curElement != NULL)
+    {
+        StackElement *nextEleement = curElement->next;
+        free(curElement);
+        curElement = nextEleement;
+    }
+
+    free(stack);
+    return 0;
+}
+
+bool isEmptyStack(Stack *stack)
+{
+    return stack->top == NULL;
+}
 bool isCorrectString(char string[])
 {
     Stack *stack = createStack();
+    if (stack == NULL)
+    {
+        printf("Memory error");
+        return -1;
+    }
     // Проверить на память
     // Возвращать конкретный код ошибки
     // Добавить проверки пуш и поп
@@ -23,7 +108,7 @@ bool isCorrectString(char string[])
             popStack(stack, &previousBracket);
             if (previousBracket != '(' || isEmptyStack(stack) && previousBracket == 0)
             {
-                printf("%c", previousBracket);
+                printf("%c) is in the string", previousBracket);
                 return false;
             }
             break;
@@ -31,6 +116,7 @@ bool isCorrectString(char string[])
             popStack(stack, &previousBracket);
             if (previousBracket != '{' || isEmptyStack(stack) && previousBracket == 0)
             {
+                printf("%c} is in the string", previousBracket);
                 return false;
             }
             break;
@@ -38,6 +124,7 @@ bool isCorrectString(char string[])
             popStack(stack, &previousBracket);
             if (previousBracket != '[' || isEmptyStack(stack) && previousBracket == 0)
             {
+                printf("(%c] is in the string", previousBracket);
                 return false;
             }
             break;
@@ -49,7 +136,7 @@ bool isCorrectString(char string[])
 // Добавить разные случаи
 bool test()
 {
-    char strings[4][7] = {"(){}[]", "((()))", "({[})]", "({})[]"};
+    char strings[6][7] = {"(){}[]", "((()))", "({[})]", "({})[]", "(a)[{}]", "aaaaaaa"};
     if (!isCorrectString(strings[0]))
     {
         return false;
@@ -63,6 +150,14 @@ bool test()
         return false;
     }
     if (!isCorrectString(strings[3]))
+    {
+        return false;
+    }
+    if (!isCorrectString(strings[4]))
+    {
+        return false;
+    }
+    if (!isCorrectString(strings[5]))
     {
         return false;
     }
